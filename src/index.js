fa17a6d81d6431628452f77060f9ad92d4400866
Jesus Vega <vegajesus1995@yahoo.com>
2020-01-21 14:23:50 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const sampleComponent = 'React and webpack are working!';
+
+ReactDOM.render(sampleComponent, rootElement);
